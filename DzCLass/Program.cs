53f711bd9d2d1f56fa3202b2ebd6c3eceb786600
Юрий Клесов <klyosovyura@gmail.com>
2020-02-11 17:37:22 +0400

    Добавьте файлы проекта.

---
+++ DzCLass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DzCLass
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+			String connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\klyos\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\Students.mdf";
+			OleDbConnection con = new OleDbConnection(connectionString);
+			List<String> Names = new List<String>();
+			using (con)
+			{
+				con.Open();
+				OleDbCommand cmd = new OleDbCommand("SELECT Name FROM Name", con);
+				OleDbDataReader reader = cmd.ExecuteReader();
+				while (reader.Read())
+				{
+					Names.Add(reader.GetString(0));
+				}
+			}
+
+			foreach (String Name in Names)
+				Console.WriteLine(Name);
+		}
+    }
+}
